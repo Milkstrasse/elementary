c2010a96a64f090eed3be09eee9f15bb549b3095
Milkstrasse <55480379+Milkstrasse@users.noreply.github.com>
2022-01-06 13:53:38 +0100

    Added message box to FightOverView

--- Magiko/Views/FightOverView.swift
+++ Magiko/Views/FightOverView.swift
@@ -34,9 +34,59 @@ struct FightOverView: View {
                             }
                             .buttonStyle(GrowingButton(width: 40))
                         }
-                        .rotationEffect(.degrees(90)).frame(width: 40, height: 170)
+                        .rotationEffect(.degrees(90)).frame(width: 40, height: 180)
                     }
-                    Spacer()
+                    HStack {
+                        HStack(spacing: 0) {
+                            ZStack(alignment: .topTrailing) {
+                                RoundedRectangle(cornerRadius: 5).fill(Color.yellow).frame(width: 109)
+                                ZStack {
+                                    Text("Game Over Messagefdfdsf").fixedSize().frame(width: 250, height: 79, alignment: .topLeading)
+                                }
+                                .frame(width: 79, height: 250).padding(.all, 15).rotationEffect(.degrees(90))
+                            }
+                            .frame(width: 109)
+                            SmallTriangle().fill(Color.yellow).frame(width: 14, height: 26).offset(y: 113).rotationEffect(.degrees(180))
+                        }
+                        Spacer()
+                        VStack {
+                            Spacer()
+                            HStack(spacing: 5) {
+                                ForEach(0 ..< 4) { index in
+                                    FighterView(fighter: leftFighters[index], isSelected: false)
+                                }
+                            }
+                            .rotationEffect(.degrees(90)).frame(width: 70, height: 295)
+                            Spacer()
+                        }
+                    }
+                    .padding(.leading, 10)
+                    Text("------- X -------").rotationEffect(.degrees(90)).fixedSize().frame(width: 60)
+                    HStack {
+                        VStack {
+                            Spacer()
+                            HStack(spacing: 5) {
+                                ForEach(0 ..< 4) { index in
+                                    FighterView(fighter: rightFighters[index], isSelected: false)
+                                }
+                            }
+                            .rotationEffect(.degrees(-90)).frame(width: 70, height: 295)
+                            Spacer()
+                        }
+                        Spacer()
+                        HStack(spacing: 0) {
+                            SmallTriangle().fill(Color.yellow).frame(width: 14, height: 26).offset(y: 113)
+                            ZStack(alignment: .bottom) {
+                                RoundedRectangle(cornerRadius: 5).fill(Color.yellow).frame(width: 109)
+                                ZStack {
+                                    Text("Game Over Messagefdfdsf").fixedSize().frame(width: 250, height: 79, alignment: .topLeading)
+                                }
+                                .frame(width: 79, height: 250).padding(.all, 15).rotationEffect(.degrees(-90))
+                            }
+                            .frame(width: 109)
+                        }
+                    }
+                    .padding(.trailing, 10)
                     VStack {
                         HStack(spacing: 5) {
                             Button("Rematch") {
@@ -50,18 +100,13 @@ struct FightOverView: View {
                             }
                             .buttonStyle(GrowingButton(width: 40))
                         }
-                        .rotationEffect(.degrees(-90)).frame(width: 40, height: 170)
+                        .rotationEffect(.degrees(-90)).frame(width: 40, height: 180)
                         Spacer()
                     }
                 }
-                .padding(.all, 15).edgesIgnoringSafeArea(.bottom)
-                HStack(spacing: 0) {
-                    LeftFightOverView(fighters: leftFighters)
-                    Text("------- X -------").rotationEffect(.degrees(90)).fixedSize().frame(width: 60)
-                    RightFightOverView(fighters: rightFighters)
-                }
-                .edgesIgnoringSafeArea(.bottom)
+                .padding(.all, 15)
             }
+            .edgesIgnoringSafeArea(.bottom)
             GeometryReader { geometry in
                 ZigZag().fill(Color.purple).frame(height: geometry.size.height + 65).rotationEffect(.degrees(180))
                     .offset(y: transitionToggle ? -65 : -(geometry.size.height + 65)).animation(.linear(duration: 0.3), value: transitionToggle).ignoresSafeArea()
@@ -69,54 +114,6 @@ struct FightOverView: View {
         }
         .onAppear {
             transitionToggle = false
-        }
-    }
-}
-
-struct LeftFightOverView: View {
-    let fighters: [Fighter?]
-    
-    var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                HStack {
-                    Spacer()
-                    VStack {
-                        Spacer()
-                        HStack(spacing: 5) {
-                            ForEach(0 ..< 4) { index in
-                                FighterView(fighter: fighters[index], isSelected: false)
-                            }
-                        }
-                        .rotationEffect(.degrees(90)).frame(width: 70, height: 295)
-                        Spacer()
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct RightFightOverView: View {
-    let fighters: [Fighter?]
-    
-    var body: some View {
-        GeometryReader { geometry in
-            ZStack(alignment: .trailing) {
-                HStack {
-                    VStack {
-                        Spacer()
-                        HStack(spacing: 5) {
-                            ForEach(0 ..< 4) { index in
-                                FighterView(fighter: fighters[index], isSelected: false)
-                            }
-                        }
-                        .rotationEffect(.degrees(-90)).frame(width: 70, height: 295)
-                        Spacer()
-                    }
-                    Spacer()
-                }
-            }
         }
     }
 }
