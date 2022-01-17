5c1b6c69e4199e155d9b0a627b675ac74e78f33a
Milkstrasse <55480379+Milkstrasse@users.noreply.github.com>
2022-01-17 11:16:10 +0100

    Moved a struct into own file

--- Magiko/Logic/DataLogic/Skill.swift
+++ Magiko/Logic/DataLogic/Skill.swift
@@ -5,8 +5,6 @@
 //  Created by Janice Hablützel on 07.01.22.
 //
 
-import Foundation
-
 struct Skill: Decodable, Hashable {
     var name: String
     let element: String
@@ -81,36 +79,5 @@ struct SubSkill: Decodable {
         weatherEffect = try container.decodeIfPresent(String.self, forKey: .weatherEffect) ?? nil
         
         healAmount = try container.decodeIfPresent(Int.self, forKey: .healAmount) ?? 0
-    }
-}
-
-struct Move {
-    let source: Fighter
-    let target: Int
-    
-    var skill: Skill
-    
-    init(source: Fighter, target: Int = -1, skill: Skill = Skill()) {
-        self.source = source
-        self.target = target
-        
-        self.skill = skill
-    }
-    
-    mutating func useSkill(amount: Int) {
-        if skill == Skill() {
-            return
-        }
-        
-        var skillIndex: Int = 0
-        for sourceSkill in source.skills {
-            if sourceSkill == skill {
-                break
-            }
-            
-            skillIndex += 1
-        }
-        skill.useCounter += amount
-        source.skills[skillIndex] = skill
     }
 }
