617c7b79faa87a90448c6481c7e970d8ba1b1126
Milkstrasse <55480379+Milkstrasse@users.noreply.github.com>
2022-11-14 11:01:57 +0100

    Restructured project

--- Elementary/Views/UIElements/Fighter.swift
+++ Elementary/Views/UIElements/Fighter.swift
@@ -1,5 +1,5 @@
 //
-//  FighterViews.swift
+//  Fighter.swift
 //  Elementary
 //
 //  Created by Janice Hablützel on 19.08.22.
---
+++ Elementary/Views/UIElements/Shapes.swift
@@ -0,0 +1,74 @@
+//
+//  Shapes.swift
+//  Elementary
+//
+//  Created by Janice Hablützel on 14.11.22.
+//
+
+import SwiftUI
+
+struct TitlePanel: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX + rect.maxY/3.35, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.closeSubpath()
+
+        return path
+    }
+}
+
+struct TriangleA: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxY * 1.65, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.closeSubpath()
+
+        return path
+    }
+}
+
+struct TriangleB: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY - rect.maxX/3.3))
+        path.closeSubpath()
+
+        return path
+    }
+}
+
+struct ZigZag: Shape {
+    func path(in rect: CGRect) -> Path {
+        let points: CGFloat = 8
+        let height: CGFloat = 50
+        
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        
+        for index in (1 ..< Int(points)) {
+            if index%2 == 0 {
+                path.addLine(to: CGPoint(x: CGFloat(index) * rect.maxX/points, y: rect.minY))
+            } else {
+                path.addLine(to: CGPoint(x: CGFloat(index) * rect.maxX/points, y: rect.minY + height))
+            }
+        }
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.closeSubpath()
+
+        return path
+    }
+}
