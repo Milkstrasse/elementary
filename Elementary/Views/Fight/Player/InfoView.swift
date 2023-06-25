dfba62ebab662aaa74ece14c079988b8ce7f2018
Milkstrasse <55480379+Milkstrasse@users.noreply.github.com>
2023-06-25 14:32:49 +0200

    Added more info to the InfoView

--- Elementary/Views/Fight/Player/InfoView.swift
+++ Elementary/Views/Fight/Player/InfoView.swift
@@ -11,6 +11,9 @@ struct InfoView: View {
     let fightLogic: FightLogic
     let player: Player
     
+    @State var isDetectingPress: Bool = false
+    @State var selectIndex: Int = -1
+    
     var body: some View {
         ScrollViewReader { value in
             VStack(spacing: General.innerPadding) {
@@ -26,7 +29,11 @@ struct InfoView: View {
                                     } else {
                                         Text(General.createSymbol(int: hex.symbol)).font(.custom("Font Awesome 5 Pro", size: General.smallFont)).foregroundColor(Color(hex.positive ? "Positive" : "Negative")).fixedSize().frame(width: 20, alignment: .leading)
                                     }
-                                    CustomText(text: Localization.shared.getTranslation(key: hex.name + "Descr"), fontSize: General.smallFont)
+                                    if isDetectingPress && selectIndex == 1 {
+                                        CustomText(text: "\(hex.duration) " + Localization.shared.getTranslation(key: "roundsLeft"), fontSize: General.smallFont)
+                                    } else {
+                                        CustomText(text: Localization.shared.getTranslation(key: hex.name + "Descr"), fontSize: General.smallFont)
+                                    }
                                     Spacer()
                                 }
                             }
@@ -40,14 +47,37 @@ struct InfoView: View {
                         .padding(.all, General.innerPadding)
                     }
                     .id(0)
+                    .onTapGesture {}
+                    .gesture(DragGesture(minimumDistance: 0)
+                        .onChanged { value in
+                            if selectIndex < 0 {
+                                selectIndex = 0
+                            }
+                            
+                            isDetectingPress = true
+                        }
+                        .onEnded({ _ in
+                            selectIndex = -1
+                            
+                            isDetectingPress = false
+                        })
+                    )
                     ZStack {
                         Rectangle().fill(Color("MainPanel")) .overlay(Rectangle().strokeBorder(Color("Border"), lineWidth: General.borderWidth))
                         VStack(alignment: .leading, spacing: 2) {
                             CustomText(text: Localization.shared.getTranslation(key: fightLogic.players[player.getOppositePlayerId()].getCurrentFighter().name).uppercased(), fontSize: General.mediumFont, isBold: true)
                             ForEach(fightLogic.players[player.getOppositePlayerId()].getCurrentFighter().hexes, id: \.self) { hex in
                                 HStack(spacing: 0) {
-                                    Text(General.createSymbol(int: hex.symbol)).font(.custom("Font Awesome 5 Pro", size: General.smallFont)).foregroundColor(Color(hex.positive ? "Positive" : "Negative")).fixedSize().frame(width: 25)
-                                    CustomText(text: Localization.shared.getTranslation(key: hex.name + "Descr"), fontSize: General.smallFont)
+                                    if hex.symbol < 60000 {
+                                        Text(General.createSymbol(int: hex.symbol)).font(.custom("Font Awesome 5 Free", size: General.smallFont)).foregroundColor(Color(hex.positive ? "Positive" : "Negative")).fixedSize().frame(width: 20, alignment: .leading)
+                                    } else {
+                                        Text(General.createSymbol(int: hex.symbol)).font(.custom("Font Awesome 5 Pro", size: General.smallFont)).foregroundColor(Color(hex.positive ? "Positive" : "Negative")).fixedSize().frame(width: 20, alignment: .leading)
+                                    }
+                                    if isDetectingPress && selectIndex == 1 {
+                                        CustomText(text: "\(hex.duration) " + Localization.shared.getTranslation(key: "roundsLeft"), fontSize: General.smallFont)
+                                    } else {
+                                        CustomText(text: Localization.shared.getTranslation(key: hex.name + "Descr"), fontSize: General.smallFont)
+                                    }
                                     Spacer()
                                 }
                             }
@@ -60,6 +90,21 @@ struct InfoView: View {
                         }
                         .padding(.all, General.innerPadding)
                     }
+                    .onTapGesture {}
+                    .gesture(DragGesture(minimumDistance: 0)
+                        .onChanged { value in
+                            if selectIndex < 0 {
+                                selectIndex = 1
+                            }
+                            
+                            isDetectingPress = true
+                        }
+                        .onEnded({ _ in
+                            selectIndex = -1
+                            
+                            isDetectingPress = false
+                        })
+                    )
                 } else {
                     VStack(spacing: General.innerPadding/2) {
                         ForEach(player.fighters.indices, id: \.self) { index in
@@ -74,7 +119,11 @@ struct InfoView: View {
                                             } else {
                                                 Text(General.createSymbol(int: hex.symbol)).font(.custom("Font Awesome 5 Pro", size: General.smallFont)).foregroundColor(Color(hex.positive ? "Positive" : "Negative")).fixedSize().frame(width: 20, alignment: .leading)
                                             }
-                                            CustomText(text: Localization.shared.getTranslation(key: hex.name + "Descr"), fontSize: General.smallFont)
+                                            if isDetectingPress && selectIndex == index {
+                                                CustomText(text: "\(hex.duration) " + Localization.shared.getTranslation(key: "roundsLeft"), fontSize: General.smallFont)
+                                            } else {
+                                                CustomText(text: Localization.shared.getTranslation(key: hex.name + "Descr"), fontSize: General.smallFont)
+                                            }
                                             Spacer()
                                         }
                                     }
@@ -88,6 +137,21 @@ struct InfoView: View {
                                 .padding(.all, General.innerPadding)
                             }
                             .id(index)
+                            .onTapGesture {}
+                            .gesture(DragGesture(minimumDistance: 0)
+                                .onChanged { value in
+                                    if selectIndex < 0 {
+                                        selectIndex = index
+                                    }
+                                    
+                                    isDetectingPress = true
+                                }
+                                .onEnded({ _ in
+                                    selectIndex = -1
+                                    
+                                    isDetectingPress = false
+                                })
+                            )
                         }
                     }
                     VStack(spacing: General.innerPadding/2) {
@@ -96,14 +160,18 @@ struct InfoView: View {
                                 Rectangle().fill(Color("MainPanel")) .overlay(Rectangle().strokeBorder(Color("Border"), lineWidth: General.borderWidth))
                                 VStack(alignment: .leading, spacing: 2) {
                                     CustomText(text: Localization.shared.getTranslation(key: fightLogic.players[player.getOppositePlayerId()].getFighter(index: index).name).uppercased(), fontSize: General.mediumFont, isBold: true)
-                                    ForEach(player.getFighter(index: index).hexes, id: \.self) { hex in
+                                    ForEach(fightLogic.players[player.getOppositePlayerId()].getFighter(index: index).hexes, id: \.self) { hex in
                                         HStack(spacing: 0) {
                                             if hex.symbol < 60000 {
                                                 Text(General.createSymbol(int: hex.symbol)).font(.custom("Font Awesome 5 Free", size: General.smallFont)).foregroundColor(Color(hex.positive ? "Positive" : "Negative")).fixedSize().frame(width: 20, alignment: .leading)
                                             } else {
                                                 Text(General.createSymbol(int: hex.symbol)).font(.custom("Font Awesome 5 Pro", size: General.smallFont)).foregroundColor(Color(hex.positive ? "Positive" : "Negative")).fixedSize().frame(width: 20, alignment: .leading)
                                             }
-                                            CustomText(text: Localization.shared.getTranslation(key: hex.name + "Descr"), fontSize: General.smallFont)
+                                            if isDetectingPress && selectIndex == player.fighters.count + index {
+                                                CustomText(text: "\(hex.duration) " + Localization.shared.getTranslation(key: "roundsLeft"), fontSize: General.smallFont)
+                                            } else {
+                                                CustomText(text: Localization.shared.getTranslation(key: hex.name + "Descr"), fontSize: General.smallFont)
+                                            }
                                             Spacer()
                                         }
                                     }
@@ -116,6 +184,20 @@ struct InfoView: View {
                                 }
                                 .padding(.all, General.innerPadding)
                             }
+                            .gesture(DragGesture(minimumDistance: 0)
+                                .onChanged { value in
+                                    if selectIndex < 0 {
+                                        selectIndex = player.fighters.count + index
+                                    }
+                                    
+                                    isDetectingPress = true
+                                }
+                                .onEnded({ _ in
+                                    selectIndex = -1
+                                    
+                                    isDetectingPress = false
+                                })
+                            )
                         }
                     }
                 }
@@ -126,7 +208,13 @@ struct InfoView: View {
                         HStack(spacing: 0) {
                             if let weather: Hex = fightLogic.weather {
                                 Text(General.createSymbol(int: weather.symbol)).font(.custom("Font Awesome 5 Pro", size: General.smallFont)).foregroundColor(Color.white).fixedSize().frame(width: 25)
-                                CustomText(text: Localization.shared.getTranslation(key: weather.name + "Descr"), fontSize: General.smallFont)
+                                if isDetectingPress && fightLogic.singleMode && selectIndex == 2 {
+                                    CustomText(text: "\(weather.duration) " + Localization.shared.getTranslation(key: "roundsLeft"), fontSize: General.smallFont)
+                                } else if isDetectingPress && selectIndex == player.fighters.count + fightLogic.players[player.getOppositePlayerId()].fighters.count {
+                                    CustomText(text: "\(weather.duration) " + Localization.shared.getTranslation(key: "roundsLeft"), fontSize: General.smallFont)
+                                } else {
+                                    CustomText(text: Localization.shared.getTranslation(key: weather.name + "Descr"), fontSize: General.smallFont)
+                                }
                             } else {
                                 CustomText(text: Localization.shared.getTranslation(key: "noWeather"), fontSize: General.smallFont)
                             }
@@ -135,6 +223,25 @@ struct InfoView: View {
                     }
                     .padding(.all, General.innerPadding)
                 }
+                .onTapGesture {}
+                .gesture(DragGesture(minimumDistance: 0)
+                    .onChanged { value in
+                        if selectIndex < 0 {
+                            if fightLogic.singleMode {
+                                selectIndex = 2
+                            } else {
+                                selectIndex = player.fighters.count + fightLogic.players[player.getOppositePlayerId()].fighters.count
+                            }
+                        }
+                        
+                        isDetectingPress = true
+                    }
+                    .onEnded({ _ in
+                        selectIndex = -1
+                        
+                        isDetectingPress = false
+                    })
+                )
             }
             .onAppear {
                 value.scrollTo(0)
